f9e8996cc5447b77b68e9242691f95d5be8b5ef5
BarberOfCoding <mihmegg@gmail.com>
2022-11-15 15:16:25 +0100

    finished project

---
+++ index.js
@@ -0,0 +1,25 @@
+const mainContainer = document.querySelector(".main-container");
+
+const thankYouContainer = document.querySelector(".thank-you");
+
+const submitButton = document.getElementById("submit");
+const rateAgainButton = document.getElementById("rate-again");
+
+const rating = document.getElementById("rating");
+const rates = document.querySelectorAll(".btn");
+
+submitButton.addEventListener("click", () => {
+  thankYouContainer.classList.remove("hidden");
+  mainContainer.style.display = "none";
+});
+
+rateAgainButton.addEventListener("click", () => {
+  thankYouContainer.classList.add("hidden");
+  mainContainer.style.display = "block";
+});
+
+rates.forEach((rate) => {
+  rate.addEventListener("click", () => {
+    rating.innerHTML = rate.innerHTML;
+  });
+});
