bd5c3df9a02738db33283798a16adb23fad49140
BarberOfCoding <94467930+BarberOfCoding@users.noreply.github.com>
2022-11-16 16:57:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,19 +8,18 @@ const rateAgainButton = document.getElementById("rate-again");
 const rating = document.getElementById("rating");
 const rates = document.querySelectorAll(".btn");
 
-submitButton.addEventListener("click", () => {
-  thankYouContainer.classList.remove("hidden");
-  mainContainer.style.display = "none";
+
+rates.forEach((rate) => {
+  rate.addEventListener("click", () => {
+    rating.innerHTML = rate.innerHTML;
+    submitButton.addEventListener("click", () => {
+       thankYouContainer.classList.remove("hidden");
+       mainContainer.style.display = "none";
+     });
+  });
 });
 
 rateAgainButton.addEventListener("click", () => {
   thankYouContainer.classList.add("hidden");
   mainContainer.style.display = "block";
 });
-
-rates.forEach((rate) => {
-  rate.addEventListener("click", () => {
-    rating.innerHTML = rate.innerHTML;
-  });
-});
-
